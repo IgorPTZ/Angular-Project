e4b84efb75e0a7276462452681cf501eaa24e1cb
Igor <igor.ptz@hotmail.com>
2021-07-02 20:40:43 -0300

    Update

--- src/app/componente/usuario/usuario-relatorio/usuario-relatorio.component.ts
+++ src/app/componente/usuario/usuario-relatorio/usuario-relatorio.component.ts
@@ -1,10 +1,8 @@
 import { Component, OnInit, Injectable } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Usuario } from 'src/app/model/usuario';
 import { UsuarioService } from 'src/app/service/usuario.service';
-import { Telefone } from 'src/app/model/telefone';
 import { NgbDateParserFormatter, NgbDateStruct, NgbDateAdapter } from '@ng-bootstrap/ng-bootstrap';
-import { Profissao } from 'src/app/model/profissao';
+import { Relatorio } from 'src/app/model/relatorio';
 
 @Injectable()
 export class FormatDateAdapter extends NgbDateAdapter<string> {
@@ -74,79 +72,13 @@ function formatarString(valor) {
   providers: [{ provide: NgbDateParserFormatter, useClass: FormatadorDeData },
   { provide: NgbDateAdapter, useClass: FormatDateAdapter }]
 })
-export class UsuarioRelatorioComponent implements OnInit {
+export class UsuarioRelatorioComponent {
 
-  usuario = new Usuario();
-
-  telefone = new Telefone();
-
-  profissoes: Array<Profissao>;
+  relatorio = new Relatorio();
 
   constructor(private routeActive: ActivatedRoute, private usuarioService: UsuarioService) { }
 
-  ngOnInit() {
-
-    this.usuarioService.consultarProfissoes().subscribe(data => {
-      this.profissoes = data;
-    });
-
-    let id = this.routeActive.snapshot.paramMap.get('id');
-
-    if (id != null) {
-      console.info('Edicao de usuario (Id:' + id + ')');
-      this.usuarioService.consultarUsuarioPeloId(id).subscribe(data => {
-        this.usuario = data;
-
-        if (this.usuario.profissao == null) {
-          this.usuario.profissao = new Profissao();
-        }
-      });
-    }
-  }
-
-  limparDadosDoUsuario() {
-    this.usuario = new Usuario();
-    this.telefone = new Telefone();
-  }
-
-  adicionarTelefone() {
-
-    if (this.usuario.telefones === undefined) {
-      this.usuario.telefones = new Array<Telefone>();
-    }
-
-    this.usuario.telefones.push(this.telefone);
-    this.telefone = new Telefone();
-  }
-
-  removerTelefone(id, index) {
-    if (id == null) {
-      this.usuario.telefones.splice(index, 1);
-      return;
-    }
-
-    if (id != null && confirm("Deseja realmente remover esse telefone?")) {
-
-      this.usuarioService.removerTelefone(id).subscribe(data => {
-        // Remove o telefone excluido no banco de dados da lista de telefones do usuario;
-        this.usuario.telefones.splice(index, 1);
-        console.info(data);
-      });
-    }
-  }
-
-  salvarUsuario() {
-
-    if (this.usuario.id != null && this.usuario.id.toString().trim() != null) {
-      this.usuarioService.editarUsuario(this.usuario).subscribe(data => {
-        this.limparDadosDoUsuario();
-        console.info("Usuario editado com sucesso! Usuario ->" + data);
-      });
-    } else {
-      this.usuarioService.criarUsuario(this.usuario).subscribe(data => {
-        this.limparDadosDoUsuario();
-        console.info("Usuario criado com sucesso! Usuario ->" + data);
-      });
-    }
+  baixarRelatorio() {
+    console.log(this.relatorio);
   }
 }
